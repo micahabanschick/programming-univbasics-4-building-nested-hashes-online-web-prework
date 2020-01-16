b5a369737c64023c7689dece3936cb823295b99d
Micah Banschick <micha.ban@gmail.com>
2020-01-16 00:56:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,9 +12,9 @@ def monopoly_with_third_tier
                   :pieces => 4,
                   rent_in_dollars: {
                     one_piece_owned: 25,
-                    b: 3,
-                    c: 4,
-                    d: 5
+                    two_pieces_owned: 50,
+                    three_pieces_owned: 75,
+                    four_pieces_owned: 100
                   },
     names: {
       z: "a",
