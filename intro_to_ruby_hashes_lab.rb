c62a97b86f403d95fb78c33ae8cbd461fb27449b
Micah Banschick <micha.ban@gmail.com>
2020-01-16 00:59:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
                   },
     names: {
       reading_railroad: {},
-      x: "a",
+      pennsylvania_railroad: {},
       c: "a",
       v: "a"
     }              
